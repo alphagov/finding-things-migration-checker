97edc602ac36b34de6efa5e16ca4751a9d9858c0
Mat Moore <mat.moore@digital.cabinet-office.gov.uk>
2016-08-03 13:12:28 +0000

    More whitelisting and improved errors
    - Corrected document type predicate
    - Throw a better error if the whitelist contains invalid predicates
    - Print the offending row if the whitelister chokes on it
    - Whitelist a known problem with withdrawn whitehall documents

--- lib/reporting/check_reporter.rb
+++ lib/reporting/check_reporter.rb
@@ -6,7 +6,7 @@ module Reporting
 
     def create_report(name, headers, candidate_rows)
       whitelister = @whitelist.get_whitelister(name, headers)
-      rows = candidate_rows.reject(&whitelister.whitelist_function)
+      rows = reject(candidate_rows, whitelister)
       unused_whitelist_entries = whitelister.unused_entries
       Reporting::Report.new(
         name: name,
@@ -25,6 +25,22 @@ module Reporting
     end
 
   private
+
+    def reject(rows, whitelister)
+      selected = []
+
+      rows.each do |row|
+        begin
+          selected << row if !whitelister.whitelist_function.call(row)
+        rescue StandardError => e
+          $stderr.puts "Error during processing: #{$!}"
+          $stderr.puts "Backtrace:\n\t#{e.backtrace.join("\n\t")}"
+          $stderr.puts "While processing row:\n\t#{row}"
+        end
+      end
+
+      selected
+    end
 
     def generate_csv(headers, rows)
       CSV.generate do |csv|
